58fbe97f161fe6536fc08076591082f2cbc567f9
Timuratmaca <115021588+Timuratmaca@users.noreply.github.com>
2024-01-15 18:06:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,19 +4,86 @@ symbols =symbols.split("")
 console.log(symbols);
 let images=[]
 let game=document.getElementById("game")
+let time=document.getElementById("time")
+let tanks=["BT-7","BT-5","BT-7M","Cy-76M","T-28e","3NC-30","BT-34","BT-343"]
+let speed=["30 km/S",] 
+// todo etot masiv derjet atkrıtıye kartoçki
+let opencards=[]
+let loading=document.getElementById("loading")
+let otk=0
 
+// todo çerez 1s zagruzka prpodayet
+setTimeout(() => {
+    loading.style.opacity="0"
+    loading.style.pointerEvents="none"
+}, 1000);
+// todo masiv imges zapalnayetsa nazvanyami kartinak
 for (let x=0;x<8;x=x+1){
     images.push("image ("+x+").png")
     images.push("image ("+x+").png")
     console.log(images);
 }
+for (let i =images.length-1;i>0;i--){
+    let randomnumber=Math.floor(Math.random()*(i+1))
+    let slot=images[i]
+    images[i]=images[randomnumber]
+    images[randomnumber]=slot
+}
+// todo sazdayot kartinki 
 for(let u=0;u<16;u=u+1){
     let card=document.createElement("img")
-    card.src=images[u]
+    card.src="card.jpg"
     game.appendChild(card)
+    card.style.height=(card.offsetWidth+"px")
+    card.onclick=function (event) {
+        card.style.pointerEvents="none"
+        // console.log("BT7num"+(u+1));
+        opencards.push(card)
+        card.style.transform="scaleX(0)"
+        setTimeout(() => {
+            card.src=images[u]
+            card.style.transform="scaleX(1)"
+            if (opencards.length==2) {
+                // console.log(opencards[0].src,opencards[1].src);
+                if (opencards[0].src==opencards[1].src) {
+                    console.log("yes");
+                    opencards=[];
+                    otk=otk+2
+                    if (otk==16){
+                        clearInterval(gametimer)
+                    }
+                }
+                else{
+                    console.log("no");
+                    let cards=game.children
+                    for(let c of cards){
+                        c.style.pointerEvents="none"
+                    }
+                    game.classList.add("shake")
+                    setTimeout(()=> {
+                        opencards[0].style.transform="scaleX(0)"
+                        opencards[1].style.transform="scaleX(0)"
+                        setTimeout(()=>{
+                            opencards[0].src="card.jpg"
+                            opencards[1].src="card.jpg"
+                            opencards[0].style.transform="scaleX(1)"  
+                            opencards[1].style.transform="scaleX(1)"  
+                            opencards=[];
+                            game.classList.remove("shake");
+                            for(let c of cards){
+                               if(c.src.includes("card.jpg") ){  c.style.pointerEvents="auto"}
+                            }
+                        },300)
+                    },1000)
+                }
+            }
+        }, 300);
+        console.log(opencards);
+        }
+        card.ondragstart=function (event) {
+            return false;
+        }
 }
-
-
 function down() {
     setInterval(()=> {
         let letter=document.createElement("span")
@@ -36,6 +103,15 @@ function down() {
 }
 down();
 
+ let t = 0;
+
+    let gametimer=setInterval(() => {
+        t=t+1
+        time.innerHTML=t
+
+    }, 1000);
+
+
 
 // newgame.onclick=function (event){
 //     event.preventDefault();
@@ -49,7 +125,10 @@ for(let o=2;o<2000;o=o*2){
 
 
 
-
+for (let t=0 ;t<9; t=t+1){
+    let tanksscore=tanks[t]+speed[0]
+    console.log(tanksscore)
+}
 
 
 
--- zapisi.c
+++ zapisi.c
@@ -1,18 +1,32 @@
-proçitat kod i abosnit kajduyu stroçku
-vreme
+sozdat knopku novaya igra 
+yesli yiyo najat to vse kartaçki zakroyutsa
 
 
 
 
+position:fixed =zafeksirovat teg na starince.teg nakladvayetsa na drugiye tegi
+// !sposab postavit absolutnıy teg po centru 
+    // !top: 50%;
+    // !left: 50%;
+    // !transform: translate(-50%,-50%);
+//! ---
+loading.style.pointerEvents="none"=nujna ştobı loading ne reagiroval na mışkuw
 
-
-
-
-
-
+floor=iz playavuyşaye çislo delayet obıçnoye çislo
+naprimer:3.12345=3 5.96=5!!!!!
 
 src=eto atribud v img gde nujna napisat nazvaniye kartinki
 createElement=zdelat teg
 push=zapehnut elemenet v masiv
 appendChild=stobı dabavit teg v drugoy teg
 font-size=razmer bukv
+object-fit=ştobı kartinki ne sjımalis
+object-fit: contain;= kartinki vidna polnastyu
+object-fit: cover;=kartinki obresayutsa
+ondragstart=kogda tı naçenayeş tenut kartinku
+game.classList.add("shake")=dobavlayet class shake
+animation=pozvalayet dobavlat animatciyu dla tega 
+@keyframes=kluçevıye kadrı
+
+
+filter: invert(1);= zdelat inversuyu cveta
